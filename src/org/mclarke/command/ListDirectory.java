1bfe3337e9bf332fe88205d9e3b7eb95bff19d81
mclarke47 <matthewclarke47@hotmail.co.uk>
2015-03-28 15:54:56 +0000

    started longlist

--- src/org/mclarke/command/ListDirectory.java
+++ src/org/mclarke/command/ListDirectory.java
@@ -3,6 +3,16 @@ package org.mclarke.command;
 import org.mclarke.Main;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,9 +22,43 @@ import java.util.stream.Stream;
 
 public class ListDirectory implements Command {
 
+    private final static Function<Map<String, File>, Map<String, File>> LongList = (m) -> {
+
+        Map<String, File> newMap = new HashMap<>();
+
+        for (String key : m.keySet()) {
+            String ownerName;
+            String size;
+            Instant lastModified;
+            BasicFileAttributes attrs;
+            Path path = Paths.get(m.get(key).getAbsolutePath());
+
+            try {
+                attrs = Files.readAttributes(path, BasicFileAttributes.class);
+                ownerName = Files.getOwner(path).getName();
+                lastModified = attrs.lastModifiedTime().toInstant();
+                size = "" + attrs.size();
+
+            } catch (IOException e) {
+                throw new RuntimeException();
+            }
+
+            if (ownerName.contains("\\")) {
+                ownerName = ownerName.split("\\\\")[1];
+            }
+
+            LocalDateTime ldt = LocalDateTime.ofInstant(lastModified, ZoneId.systemDefault());
+
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MMM dd HH:mm");
+
+            String line = ownerName + " " + " group " + "  " + size + " " + dateTimeFormatter.format(ldt) + " " + m.get(key).getName() + "\n";
+
+            newMap.put(line, m.get(key));
+        }
+        return newMap;
+    };
     private Map<String, Function<Map<String, File>,Map<String, File>>> options = new HashMap<>();
     private List<Function<Map<String, File>,Map<String, File>>> flagsOperations;
-
     private Function<File, String> getName = (f) -> {
         if (f.isDirectory()) {
             return f.getName() + "/";
@@ -24,7 +68,7 @@ public class ListDirectory implements Command {
     };
 
     public ListDirectory(){
-        //options.put("l", (s) -> s);
+        options.put("l", LongList);
     }
 
     @Override
@@ -38,6 +82,7 @@ public class ListDirectory implements Command {
         }
 
         flagsOperations = Stream.of(params).map(stripHyphen).map(options::get).collect(Collectors.toList());
+
 
     }
 
@@ -53,6 +98,9 @@ public class ListDirectory implements Command {
         //TODO boiler plate done, implement solution
         //TODO replace map with List of a new type? Map doesn't seem right for some reason
 
-        Stream.of(displayStringToFile.keySet()).forEach(System.out::println);
+        List<String> formattedFiles = new ArrayList<>();
+        formattedFiles.addAll(displayStringToFile.keySet());
+
+        formattedFiles.stream().forEach(System.out::print);
     }
 }
